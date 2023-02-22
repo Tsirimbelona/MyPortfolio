c55e5fd803a65e6663d200b37baf586cf472e024
Tsirimbelona <jeannicotsirimbelona17@gmail.com>
2023-02-21 21:39:23 -0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ====================== typing animation ================ */ 
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web developer","Graphic Designer","You Tuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
